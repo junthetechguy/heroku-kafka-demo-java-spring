59cbccd7c3c843a1d20a207599c1d04faa6ab14c
junthetechguy <forestwoodleaves@gmail.com>
2024-07-05 15:21:00 +0900

    init: Initialize project

---
+++ src/main/java/com/heroku/kafka/demo/HerokuKafkaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.heroku.kafka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuKafkaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuKafkaDemoApplication.class, args);
+	}
+
+}
